b3166f4448ecc4140a4ed37a33ceb5492f68f806
Prerana Ramesh Bhandari <prerana.bhandari99@gmail.com>
2024-06-22 15:53:05 +0530

    Adding "delete note" functionality to iNotebook

--- src/App.js
+++ src/App.js
@@ -4,33 +4,32 @@ import {
   Switch,
   Route
 } from "react-router-dom";
-
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About';
 import NoteState from './context/notes/NoteState';
-import Alert from './components/Alert';
-  
+import { Alert } from './components/Alert';
+
 function App() {
   return (
-    <> 
-    <NoteState>
-    <Router>
-      <Navbar />
-      <Alert message="This is amazing react course."/>
-      <div className="container">
-        <Switch>
-          <Route exact path="/">
-            <Home/>
-          </Route>
-          <Route exact path="/about">
-            <About/>
-          </Route> 
-        </Switch>
-      </div> 
-      </Router> 
+    <>
+      <NoteState>
+        <Router>
+          <Navbar />
+          <Alert message="This is amazing React course" />
+          <div className="container">
+            <Switch>
+              <Route exact path="/">
+                <Home />
+              </Route>
+              <Route exact path="/about">
+                <About />
+              </Route>
+            </Switch>
+          </div>
+        </Router>
       </NoteState>
-  </>
+    </>
   );
 }
 
